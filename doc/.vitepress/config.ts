347dffe75150d7878383adeee155b9059aefb040
Binbiubiubiu <2632003786@qq.com>
2023-03-16 01:52:35 +0800

    fix: fix base

--- doc/.vitepress/config.ts
+++ doc/.vitepress/config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from "vitepress";
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
+  base: "/tqt-doc/",
   title: "TQT",
   description: "tb-mini & qn-mini & taro",
   themeConfig: {
